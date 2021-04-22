03ce0b8fad8adf492033d5d939dced54a31d1895
David Robison <david@robisonweb.dev>
2021-04-22 14:09:41 -0600

    Created GeneralInfo class which will render gerenal info

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,1 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+class GeneralInfo extends Component {
+    render() {
+        const { name, city, state, zipCode, phone, email } = this.state;
+
+        return (
+            <div>
+                <h1>{name}</h1>
+                <p>{city}, {state}, {zipCode}</p>
+                <p>{phone}</p>
+                <p>{email}</p>
+            </div>
+        );
+    }
+}
+
+export default GeneralInfo;
