f7e20355caf151d97645d005abadd8375e0a7782
David Robison <david@robisonweb.dev>
2021-04-23 12:31:23 -0600

    Refactored code

--- src/components/InputGeneralInfo.js
+++ src/components/InputGeneralInfo.js
@@ -7,27 +7,9 @@ class InputGeneralInfo extends Component {
 
     render() {
         return (
-            <div id='InputGeneralInfo'>
-                <div className='inputField'>
-                    <label htmlFor='fullname'>Fullname</label>
-                    <input type='text' name='fullname' id='fullname' onChange={(e) => this.props.handleChange(e)}></input>
-                </div>
-                <div className='InputGeneralInfo'>
-                    <label htmlFor='city'>City</label>
-                    <input type='text' name='city' id='city' onChange={(e) => this.props.handleChange(e)}></input>
-                </div>
-                <div className='InputGeneralInfo'>
-                    <label htmlFor='state'>State</label>
-                    <input type='text' name='state' id='state' onChange={(e) => this.props.handleChange(e)}></input>
-                </div>
-                <div className='InputGeneralInfo'>
-                    <label htmlFor='phone'>Phone Number</label>
-                    <input type='text' name='phone' id='phone' onChange={(e) => this.props.handleChange(e)}></input>
-                </div>
-                <div className='InputGeneralInfo'>
-                    <label htmlFor='email'>Email</label>
-                    <input type='text' name='email' id='email' onChange={(e) => this.props.handleChange(e)}></input>
-                </div>
+            <div className='inputField'>
+                <label htmlFor={this.props.name}>{this.props.label}</label>
+                <input type='text' name={this.props.name} id={this.props.name} onChange={(e) => this.props.handleChange(e)}></input>
             </div>
         );
     }
