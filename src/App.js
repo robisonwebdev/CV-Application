714918a276f62ec4a0fc26cc1b955f46537339cb
David Robison <david@robisonweb.dev>
2021-04-23 12:32:50 -0600

    Returned CVForm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import CVForm from './components/CVForm';
 
 class App extends Component {
   render() {
     return (
-      <div>CV APP</div>
+      <CVForm />
     );
   }
 }
