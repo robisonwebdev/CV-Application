a28b7eddb3c663a9a4abcd55b9b5539d146a82c9
David Robison <david@robisonweb.dev>
2021-04-23 10:17:28 -0600

    Setup handleSubmit method

--- src/components/CVForm.js
+++ src/components/CVForm.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import InputGeneralInfo from './InputGeneralInfo';
+import cvData from '../cvData';
 
 class CVForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: cvData,
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log('Submit button clicked');
+    }
+
     render() {
         return (
             <div id='CVForm'>
-
+                <form>
+                    <InputGeneralInfo />
+                    <button onClick={this.handleSubmit}>Submit</button>
+                </form>
             </div>
         );
     }
