5d5d6314dbea56fa58216bc7bc7ef3ea34268f3d
David Robison <david@robisonweb.dev>
2021-04-24 11:25:16 -0600

    Added onChange and props to constructor/super

--- src/components/PersonalInformation.js
+++ src/components/PersonalInformation.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 
 class PersonalInformation extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
     }
 
     render() {
         return (
             <div id='personalInfo'>
-                <input type='text' id='firstName' placeholder='First name' />
-                <input type='text' id='lastName' placeholder='Last name' />
-                <input type='text' id='city' placeholder='City' />
-                <input type='text' id='state' placeholder='State' />
-                <input type='text' id='zipCode' placeholder='Zip code' />
-                <input type='tel' id='phoneNumber' placeholder='Phone number' />
-                <input type='email' id='email' placeholder='Email' />
+                <input type='text' id='firstName' placeholder='First name' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' id='lastName' placeholder='Last name' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' id='city' placeholder='City' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' id='state' placeholder='State' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' id='zipCode' placeholder='Zip code' onChange={(e) => this.props.handleChange(e)} />
+                <input type='tel' id='phoneNumber' placeholder='Phone number' onChange={(e) => this.props.handleChange(e)} />
+                <input type='email' id='email' placeholder='Email' onChange={(e) => this.props.handleChange(e)} />
             </div>
         );
     }
