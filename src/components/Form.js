f3fd063fbcca39d5ad5279dbb2855c0faf4bbc17
David Robison <david@robisonweb.dev>
2021-04-24 11:26:18 -0600

    Added/bind handleChange and handleSubmit, passed to PersonalInformation

---
+++ src/components/Form.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PersonalInformation from './PersonalInformation';
+
+class Form extends Component {
+    constructor() {
+        super()
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.state = {};
+    }
+
+    handleChange(e) {
+        const target = e.target;
+        const value = target.value;
+        const name = target.id;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(this.state)
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <PersonalInformation handleChange={this.handleChange} />
+                    <button type='submit' value='Submit'>Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Form;
