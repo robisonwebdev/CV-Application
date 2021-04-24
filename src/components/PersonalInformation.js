dddbda274eb96e19e0a9096ff4dfd41ecc5c2f47
David Robison <david@robisonweb.dev>
2021-04-24 11:16:53 -0600

    Added constructor and super

--- src/components/PersonalInformation.js
+++ src/components/PersonalInformation.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class PersonalInformation extends Component {
+    constructor() {
+        super()
+    }
+
     render() {
         return (
             <div id='personalInfo'>
