4f9882b3a5be795c6195e47a0b383d8d4177b864
David Robison <david@robisonweb.dev>
2021-04-24 09:45:10 -0600

    Class setup

---
+++ src/components/InputWorkHistory.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class InputWorkHistory extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>Hello</div>
+        );
+    }
+}
+
+export default InputWorkHistory;
