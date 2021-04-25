91300c2ac6305c3f330afd2168aa8bd7faa90d54
David Robison <david@robisonweb.dev>
2021-04-25 10:39:12 -0600

    Changed ID to name

--- src/components/PersonalInformation.js
+++ src/components/PersonalInformation.js
@@ -9,13 +9,13 @@ class PersonalInformation extends Component {
         return (
             <div id='personalInfo'>
                 <h1>Personal Information</h1>
-                <input type='text' id='firstName' placeholder='First name' onChange={(e) => this.props.handleChange(e)} />
-                <input type='text' id='lastName' placeholder='Last name' onChange={(e) => this.props.handleChange(e)} />
-                <input type='text' id='city' placeholder='City' onChange={(e) => this.props.handleChange(e)} />
-                <input type='text' id='state' placeholder='State' onChange={(e) => this.props.handleChange(e)} />
-                <input type='text' id='zipCode' placeholder='Zip code' onChange={(e) => this.props.handleChange(e)} />
-                <input type='tel' id='phoneNumber' placeholder='Phone number' onChange={(e) => this.props.handleChange(e)} />
-                <input type='email' id='email' placeholder='Email' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' name='firstName' placeholder='First name' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' name='lastName' placeholder='Last name' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' name='city' placeholder='City' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' name='state' placeholder='State' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' name='zipCode' placeholder='Zip code' onChange={(e) => this.props.handleChange(e)} />
+                <input type='tel' name='phoneNumber' placeholder='Phone number' onChange={(e) => this.props.handleChange(e)} />
+                <input type='email' name='email' placeholder='Email' onChange={(e) => this.props.handleChange(e)} />
             </div>
         );
     }
