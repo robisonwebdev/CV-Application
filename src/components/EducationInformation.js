a15c2f904f815306e1cbc896644d6e2c84451e81
David Robison <david@robisonweb.dev>
2021-04-25 11:32:15 -0600

    Setup class, constructor, and render

---
+++ src/components/EducationInformation.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class EducationInformation extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div id='educationInformation'>
+
+            </div>
+        );
+    }
+}
+
+export default EducationInformation;
