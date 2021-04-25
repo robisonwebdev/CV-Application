44d1bdc06dd24ed67a9e0470135976902d4dfcee
David Robison <david@robisonweb.dev>
2021-04-25 11:38:50 -0600

    Added inputs for education information and passed down props to handle change

--- src/components/EducationInformation.js
+++ src/components/EducationInformation.js
@@ -8,7 +8,12 @@ class EducationInformation extends Component {
     render() {
         return (
             <div id='educationInformation'>
-
+                <input type='text' name='school' placeholder='School' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' name='major' placeholder='Major' onChange={(e) => this.props.handleChange(e)} />
+                <div>
+                    <label htmlFor='graduationDate'>Graduation Date: </label>
+                    <input type='date' name='graduationDate' onChange={(e) => this.props.handleChange(e)} />
+                </div>
             </div>
         );
     }
