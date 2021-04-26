94936b15344bcf0d1340f1d57642cafd46a136aa
David Robison <david@robisonweb.dev>
2021-04-26 10:42:43 -0600

    Moved Form in div, and added CVHeader and CVFooter. Set div to ID 'app'.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
+import './styles/main.css'
+import './styles/cvForm.css';
+import CVHeader from './components/CVHeader';
+import CVFooter from './components/CVFooter';
 
 class App extends Component {
   render() {
     return (
-      <Form />
+      <div id='app'>
+        <CVHeader />
+        <Form />
+        <CVFooter />
+      </div>
+
     );
   }
 }
