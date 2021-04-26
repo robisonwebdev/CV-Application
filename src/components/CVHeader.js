54b6aea88a9abacf4ddc3cfcdb5cb8bc85a1a560
David Robison <david@robisonweb.dev>
2021-04-26 10:16:14 -0600

    Function for CV Header, contains icon and text

---
+++ src/components/CVHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CVHeader = () => {
+    return (
+        <div id='cvHeader'>
+            <i class="las la-file"></i>
+            <p>CV Maker</p>
+        </div>
+    );
+}
+
+export default CVHeader;
