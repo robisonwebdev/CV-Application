8967485f54c8307092cc865c1b56863037a636c4
David Robison <david@robisonweb.dev>
2021-04-26 10:41:12 -0600

    Created footer function

---
+++ src/components/CVFooter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CVFooter = () => {
+    return (
+        <div id='cvFooter'>
+            <footer>
+                <small>
+                    Developed by <a href='https://github.com/robisonwebdev' target='_blank'>David Robison</a>
+                </small>
+            </footer>
+        </div>
+    );
+}
+
+export default CVFooter;
