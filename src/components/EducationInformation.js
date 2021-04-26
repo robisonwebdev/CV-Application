55b8195c69131d0227426bd0d32a9bcbc29c6d6a
David Robison <david@robisonweb.dev>
2021-04-26 09:53:42 -0600

    Added input of text for university city

--- src/components/EducationInformation.js
+++ src/components/EducationInformation.js
@@ -8,7 +8,8 @@ class EducationInformation extends Component {
     render() {
         return (
             <div id='educationInformation'>
-                <input type='text' name='university' placeholder='University Name' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' name='university' placeholder='University' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' name='universityCity' placeholder='City' onChange={(e) => this.props.handleChange(e)} />
                 <input type='text' name='degree' placeholder='Degree' onChange={(e) => this.props.handleChange(e)} />
                 <input type='text' name='attendedFrom' placeholder='From' onChange={(e) => this.props.handleChange(e)} />
                 <input type='text' name='attendedTo' placeholder='To' onChange={(e) => this.props.handleChange(e)} />
