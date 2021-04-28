2572e34748da6fa4daea4757dee2fc1fce7282e0
David Robison <david@robisonweb.dev>
2021-04-28 17:17:11 -0600

    Added div with ID of mainContent, and set Form and CVLetter between div. Imported cvLetter.css

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import Form from './components/Form';
 import './styles/main.css'
 import './styles/cvForm.css';
+import './styles/cvLetter.css';
 import CVHeader from './components/CVHeader';
 import CVFooter from './components/CVFooter';
+import CVLetter from './components/CVLetter';
 
 class App extends Component {
   render() {
     return (
       <div id='app'>
         <CVHeader />
-        <Form />
+        <div id='mainContent'>
+          <Form />
+          <CVLetter />
+        </div>
         <CVFooter />
       </div>
 
