5f1edd33585d6626c79189c6fa99929cd289eec5
David Robison <david@robisonweb.dev>
2021-04-29 10:39:27 -0600

    Added data attribute to inputs

--- src/components/PersonalInformation.js
+++ src/components/PersonalInformation.js
@@ -9,13 +9,13 @@ class PersonalInformation extends Component {
         return (
             <div id='personalInfo'>
                 <h1 className='cvFormHeaders'>Personal Information</h1>
-                <input type='text' name='firstName' placeholder='First name' onChange={(e) => this.props.handleChange(e)} />
-                <input type='text' name='lastName' placeholder='Last name' onChange={(e) => this.props.handleChange(e)} />
-                <input type='text' name='city' placeholder='City' onChange={(e) => this.props.handleChange(e)} />
-                <input type='text' name='state' placeholder='State' onChange={(e) => this.props.handleChange(e)} />
-                <input type='text' name='zipCode' placeholder='Zip code' onChange={(e) => this.props.handleChange(e)} />
-                <input type='tel' name='phoneNumber' placeholder='Phone number' onChange={(e) => this.props.handleChange(e)} />
-                <input type='email' name='email' placeholder='Email' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' data-state='personalInfo' name='firstName' placeholder='First name' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' data-state='personalInfo' name='lastName' placeholder='Last name' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' data-state='personalInfo' name='city' placeholder='City' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' data-state='personalInfo' name='state' placeholder='State' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' data-state='personalInfo' name='zipCode' placeholder='Zip code' onChange={(e) => this.props.handleChange(e)} />
+                <input type='tel' data-state='personalInfo' name='phoneNumber' placeholder='Phone number' onChange={(e) => this.props.handleChange(e)} />
+                <input type='email' data-state='personalInfo' name='email' placeholder='Email' onChange={(e) => this.props.handleChange(e)} />
             </div>
         );
     }
