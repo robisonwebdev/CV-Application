f1a80cc6aa0e37d1e00f2f23ad3a042ff58a4af7
David Robison <david@robisonweb.dev>
2021-04-29 10:37:11 -0600

    Moved the deleted code to App.ja. changed the onSubmit to work with passed down prop. changed the handleChange to work with passed down props.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,75 +1,21 @@
 import React, { Component } from 'react';
+import CVLetter from './CVLetter';
 import Education from './Education';
 import Employment from './Employment';
 import PersonalInformation from './PersonalInformation';
 
 class Form extends Component {
-    constructor() {
-        super()
-
-        this.handlePersonalInfoChange = this.handlePersonalInfoChange.bind(this);
-        this.handleEmploymentChange = this.handleEmploymentChange.bind(this);
-        this.handleEducationChange = this.handleEducationChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-
-        this.state = {
-            personalInfo: {},
-            employment: {},
-            education: {},
-        };
-    }
-
-    handlePersonalInfoChange(e) {
-        const target = e.target;
-        const value = target.value;
-        const name = target.name;
-
-        this.setState((prevState) => ({
-            personalInfo: {
-                ...prevState.personalInfo,
-                [name]: value
-            }
-        }))
-    }
-
-    handleEmploymentChange(e) {
-        const target = e.target;
-        const value = target.value;
-        const name = target.name;
-
-        this.setState((prevState) => ({
-            employment: {
-                ...prevState.employment,
-                [name]: value
-            }
-        }))
-    }
-
-    handleEducationChange(e) {
-        const target = e.target;
-        const value = target.value;
-        const name = target.name;
-
-        this.setState((prevState) => ({
-            education: {
-                ...prevState.education,
-                [name]: value
-            }
-        }))
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        console.log(this.state)
+    constructor(props) {
+        super(props);
     }
 
     render() {
         return (
             <div id='cvForm'>
-                <form onSubmit={this.handleSubmit}>
-                    <PersonalInformation handleChange={this.handlePersonalInfoChange} />
-                    <Employment handleChange={this.handleEmploymentChange} />
-                    <Education handleChange={this.handleEducationChange} />
+                <form onSubmit={this.props.handleSubmit}>
+                    <PersonalInformation handleChange={this.props.handleChange} />
+                    <Employment handleChange={this.props.handleChange} />
+                    <Education handleChange={this.props.handleChange} />
                     <div id='submitForm'><button type='submit' id='btnSubmit' value='Submit'>Submit</button></div>
                 </form>
             </div>
