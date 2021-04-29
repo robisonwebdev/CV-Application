597c730c7c9cf66c52ab702e93625e37547e557f
David Robison <david@robisonweb.dev>
2021-04-29 10:41:32 -0600

    Moved state from Form.js into App.js Reduced the number of handle functions from 3 to 1. Passed state down into Form.

--- src/App.js
+++ src/App.js
@@ -8,13 +8,45 @@ import CVFooter from './components/CVFooter';
 import CVLetter from './components/CVLetter';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      personalInfo: {},
+      employment: {},
+      education: {},
+    };
+  }
+
+  handleChange(e) {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+    const data = target.dataset.state
+
+    this.setState((prevState) => ({
+      [data]: {
+        ...prevState[data],
+        [name]: value
+      }
+    }))
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div id='app'>
         <CVHeader />
         <div id='mainContent'>
-          <Form />
-          <CVLetter />
+          <Form handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
+          <CVLetter data={this.state.education} />
         </div>
         <CVFooter />
       </div>
