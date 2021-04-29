f00b395465850d508770fb1dae052b5b26aea1be
David Robison <david@robisonweb.dev>
2021-04-29 10:39:16 -0600

    Added data attribute to inputs

--- src/components/EducationInformation.js
+++ src/components/EducationInformation.js
@@ -8,11 +8,11 @@ class EducationInformation extends Component {
     render() {
         return (
             <div id='educationInformation'>
-                <input type='text' name='university' placeholder='University' onChange={(e) => this.props.handleChange(e)} />
-                <input type='text' name='universityCity' placeholder='City' onChange={(e) => this.props.handleChange(e)} />
-                <input type='text' name='degree' placeholder='Degree' onChange={(e) => this.props.handleChange(e)} />
-                <input type='text' name='attendedFrom' placeholder='From' onChange={(e) => this.props.handleChange(e)} />
-                <input type='text' name='attendedTo' placeholder='To' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' data-state='education' name='university' placeholder='University' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' data-state='education' name='universityCity' placeholder='City' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' data-state='education' name='degree' placeholder='Degree' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' data-state='education' name='attendedFrom' placeholder='From' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' data-state='education' name='attendedTo' placeholder='To' onChange={(e) => this.props.handleChange(e)} />
             </div>
         );
     }
