590f54a5fa3adaada34e43fd1d8aaabb4f71785f
David Robison <david@robisonweb.dev>
2021-04-30 10:12:45 -0600

    Imported React and setup basics for class PreviewHeader

---
+++ src/components/PreviewHeader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PreviewHeader extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>Header</div>
+        );
+    }
+}
+
+export default PreviewHeader;
