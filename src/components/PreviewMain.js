2c33ac89e8cad4b0e348a355e8fbea1d42ccc922
David Robison <david@robisonweb.dev>
2021-04-30 10:16:27 -0600

    Imported React, setup basics for class PreviewMain and exported

---
+++ src/components/PreviewMain.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PreviewMain extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>Main Content</div>
+        );
+    }
+}
+
+export default PreviewMain;
