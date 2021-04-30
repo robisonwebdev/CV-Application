cc934d7660c5f2e7d4f9c1be7344d30b9eb510d0
David Robison <david@robisonweb.dev>
2021-04-30 10:25:06 -0600

    Imported React, setup basics for class PreviewSideInfo and exported

---
+++ src/components/PreviewSideInfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PreviewSideInfo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>Side Info</div>
+        );
+    }
+}
+
+export default PreviewSideInfo;
