2e0f865921f3a65d2db43eeda3c205833f97d455
David Robison <david@robisonweb.dev>
2021-05-03 16:42:56 -0600

    Added city input

--- src/components/EmploymentInformation.js
+++ src/components/EmploymentInformation.js
@@ -10,6 +10,7 @@ class EmploymentInformation extends Component {
             <div id='employmentInformation'>
                 <input type='text' data-state='employment' name='position' placeholder='Position' onChange={(e) => this.props.handleChange(e)} />
                 <input type='text' data-state='employment' name='company' placeholder='Company' onChange={(e) => this.props.handleChange(e)} />
+                <input type='text' data-state='employment' name='companyCity' placeholder='City' onChange={(e) => this.props.handleChange(e)} />
                 <input type='text' data-state='employment' name='fromDate' placeholder='From' onChange={(e) => this.props.handleChange(e)} />
                 <input type='text' data-state='employment' name='toDate' placeholder='To' onChange={(e) => this.props.handleChange(e)} />
             </div>
