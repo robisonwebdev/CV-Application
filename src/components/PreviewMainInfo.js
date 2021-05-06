aaad91ceb6f939da2798a28bfada425ea5303179
David Robison <david@robisonweb.dev>
2021-05-05 19:17:39 -0600

    Added className of previewDescription to div element holding description

--- src/components/PreviewMainInfo.js
+++ src/components/PreviewMainInfo.js
@@ -15,7 +15,7 @@ class PreviewMainInfo extends Component {
                 <div className='mainInfoItems'>
                     <h3 className='cvSubTitle'>Description</h3>
                     <hr></hr>
-                    <div>{description}</div>
+                    <div className='previewDescription'>{description}</div>
                 </div>
                 <div className='mainInfoItems'>
                     <h3 className='cvSubTitle'>Experience</h3>
