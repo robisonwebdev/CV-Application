83408016e2d4e09a8f75a70844298c39ec3c50db
David Robison <david@robisonweb.dev>
2021-05-07 15:57:29 -0600

    Imported React. Setup class PreviewEmploymentInfo, with constructor and render. Export PreviewEmploymentInfo.

---
+++ src/components/PreviewEmploymentInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PreviewEmploymentInfo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        <div></div>
+    }
+}
+
+export default PreviewEmploymentInfo;
