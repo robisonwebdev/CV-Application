823f69a291a2105bf5a2ce2cf6d2e9f4176ee551
David Robison <david@robisonweb.dev>
2021-05-06 18:29:22 -0600

    Removed Experience code and replaced it with PreviewEmployment component.

--- src/components/PreviewMainInfo.js
+++ src/components/PreviewMainInfo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Description from './Description';
+import PreviewEmployment from './PreviewEmployment';
 
 class PreviewMainInfo extends Component {
     constructor(props) {
@@ -7,24 +8,14 @@ class PreviewMainInfo extends Component {
     }
 
     render() {
-        const { fromDate, toDate, position, company, companyCity } = this.props.data.employment;
+
         const { attendedFrom, attendedTo, degree, university, universityCity } = this.props.data.education;
 
 
         return (
             <div id="mainInfo">
                 <Description data={this.props.data} />
-                <div className='mainInfoItems'>
-                    <h3 className='cvSubTitle'>Experience</h3>
-                    <hr></hr>
-                    <div className='previewEmploymentInfo'>
-                        <div>{`${fromDate || ''} - ${toDate || ''}`}</div>
-                        <div className='positionCompany'>
-                            <div>{position || ''}</div>
-                            <div>{`${company || ''}, ${companyCity || ''}`}</div>
-                        </div>
-                    </div>
-                </div>
+                <PreviewEmployment />
                 <div className='mainInfoItems'>
                     <h3 className='cvSubTitle'>Education</h3>
                     <hr></hr>
