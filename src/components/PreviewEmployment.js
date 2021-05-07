b7de5f1a8bd7b295fd9586fac5ea3808dcf22a76
David Robison <david@robisonweb.dev>
2021-05-06 18:27:18 -0600

    Copied Experience code from PreviewMainInfo.js and paste it into PreviewEmployment.

--- src/components/PreviewEmployment.js
+++ src/components/PreviewEmployment.js
@@ -7,7 +7,17 @@ class PreviewEmployment extends Component {
 
     render() {
         return (
-            <div className='mainInfoItems'></div>
+            <div className='mainInfoItems'>
+                <h3 className='cvSubTitle'>Experience</h3>
+                <hr></hr>
+                <div className='previewEmploymentInfo'>
+                    <div>{`${fromDate || ''} - ${toDate || ''}`}</div>
+                    <div className='positionCompany'>
+                        <div>{position || ''}</div>
+                        <div>{`${company || ''}, ${companyCity || ''}`}</div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
