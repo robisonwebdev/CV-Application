e639fe8e30a716dad50b3a5c8d4d881a671bc730
David Robison <david@robisonweb.dev>
2021-05-08 17:06:55 -0600

    Removed sideInfo code

--- src/components/PreviewSideContainer.js
+++ src/components/PreviewSideContainer.js
@@ -12,18 +12,6 @@ class PreviewSideContainer extends Component {
             <div id="sideInfoContainer">
                 <h3>Personal Details</h3>
                 <hr></hr>
-                <div>
-                    <h4>Address</h4>
-                    <p>{`${city || 'City'}, ${state || 'State'} ${zipCode || 'Zip'}`}</p>
-                </div>
-                <div>
-                    <h4>Phone Number</h4>
-                    <p>{phoneNumber || 'xxx-xxx-xxxx'}</p>
-                </div>
-                <div>
-                    <h4>Email</h4>
-                    <p>{email || 'placeholder@email.com'}</p>
-                </div>
             </div>
         );
     }
