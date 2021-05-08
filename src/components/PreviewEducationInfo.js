fa87e49f3af1df494b376d95c7dfa975840f761f
David Robison <david@robisonweb.dev>
2021-05-08 16:49:55 -0600

    Imported React. Setup PreviewEducationClass. Exported PreviewEducationClass.

---
+++ src/components/PreviewEducationInfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PreviewEducationInfo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default PreviewEducationInfo;
