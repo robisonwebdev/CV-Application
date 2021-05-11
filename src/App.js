f784acf38b2079c882d63699c493eac59705f123
David Robison <david@robisonweb.dev>
2021-05-11 14:31:41 -0600

    Commented out class App. Created functional App component. Imported useState.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Form from './components/Form';
 import './styles/main.css'
 import './styles/cvForm.css';
@@ -7,49 +7,56 @@ import CVHeader from './components/CVHeader';
 import CVFooter from './components/CVFooter';
 import PreviewCV from './components/PreviewCV';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
 
-    this.state = {
-      personalInfo: {},
-      employment: {},
-      education: {},
-    };
-  }
+//     this.state = {
+//       personalInfo: {},
+//       employment: {},
+//       education: {},
+//     };
+//   }
 
-  handleChange(e) {
-    const target = e.target;
-    const value = target.value;
-    const name = target.name;
-    const data = target.dataset.state
+//   handleChange(e) {
+//     const target = e.target;
+//     const value = target.value;
+//     const name = target.name;
+//     const data = target.dataset.state
 
-    this.setState((prevState) => ({
-      [data]: {
-        ...prevState[data],
-        [name]: value
-      }
-    }))
-  }
+//     this.setState((prevState) => ({
+//       [data]: {
+//         ...prevState[data],
+//         [name]: value
+//       }
+//     }))
+//   }
 
+//   handleSubmit(e) {
+//     e.preventDefault();
+//     console.log(this.state)
+//   }
 
+//   render() {
+//     return (
+//       <div id='app'>
+//         <CVHeader />
+//         <div id='mainContent'>
+//           <Form handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
+//           <PreviewCV data={this.state} />
+//         </div>
+//         <CVFooter />
+//       </div>
 
-  render() {
-    return (
-      <div id='app'>
-        <CVHeader />
-        <div id='mainContent'>
-          <Form handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
-          <PreviewCV data={this.state} />
-        </div>
-        <CVFooter />
-      </div>
+//     );
+//   }
+// }
 
-    );
-  }
+const App = () => {
+
 }
 
 export default App;
