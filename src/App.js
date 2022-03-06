de1c5653c77b9cadfc032287a96084bedaf5c555
robisonwebdev <david@robisonweb.dev>
2022-03-06 12:10:54 -0700

    Changed App from class component to functional component. Removed all imports but React and App.css Imported Main component.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,13 @@
-import React, { Component } from 'react';
-import Form from './components/Form';
+import React from 'react';
+import Main from './components/Main/Main';
 import './styles/App.css';
-import './styles/main.css'
-import './styles/cvForm.css';
-import './styles/previewCV.css';
-import CVHeader from './components/CVHeader';
-import CVFooter from './components/CVFooter';
-import PreviewCV from './components/PreviewCV';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-
-    this.state = {
-      personalInfo: {},
-      employment: {},
-      education: {},
-    };
-  }
-
-  handleChange(e) {
-    const target = e.target;
-    const value = target.value;
-    const name = target.name;
-    const data = target.dataset.state
-
-    this.setState((prevState) => ({
-      [data]: {
-        ...prevState[data],
-        [name]: value
-      }
-    }))
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    console.log(this.state)
-  }
-
-  render() {
-    return (
-      <div id='app'>
-        <CVHeader />
-        <div id='mainContent'>
-          <Form handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
-          <PreviewCV data={this.state} />
-        </div>
-        <CVFooter />
-      </div>
-
-    );
-  }
-}
+const App = () => {
+  return (
+    <div id='App'>
+      <Main />
+    </div>
+  );
+};
 
 export default App;
