a017c3373c7f3cb39e56ea698f661d97d2bf105a
robisonwebdev <david@robisonweb.dev>
2022-03-07 12:44:50 -0700

    Imported useState and Button component. Setup state for buttonText, showForm, and showPreview. Setup handleButton function to flip boolen values of showForm and showPreview. Also to change Button text. Added div with className of view_switch to main. Inside .view_switch, called Button component. Passed Button component handleButton and buttonText. Added empty section tag to main.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from './Button';
 import '../../styles/Main/Main.css';
 
 const Main = () => {
+    const [buttonText, setButtonText] = useState('Preview');
+    const [showForm, setShowForm] = useState(true);
+    const [showPreview, setShowPreview] = useState(false);
+
+    const handleButton = () => {
+        setShowForm(!showForm);
+        setShowPreview(!showPreview);
+        
+        {buttonText === 'Preview' ? setButtonText('Edit') : setButtonText('Preview')}
+    };
+
     return (
-        <main>Main</main>
+        <main>
+            <div className='view_switch'>
+                <Button handleButton={handleButton} text={buttonText} />
+            </div>
+            <section>
+                
+            </section>
+        </main>
     );
 };
 
