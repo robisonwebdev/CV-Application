3f6034d907523cb83f50bca6d418b68e481c3cca
robisonwebdev <david@robisonweb.dev>
2022-03-07 12:45:35 -0700

    Added handleButton and text props. Setup onClick event on button, passing it handleButton. Added text prop between button tag.

--- src/components/Main/Button.js
+++ src/components/Main/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Button = () => {
+const Button = ({ handleButton, text }) => {
     return (
-        <button>Button</button>
+        <button onClick={handleButton}>{text}</button>
     );
 };
 
