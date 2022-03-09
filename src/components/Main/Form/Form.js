10091192f986e39cc2dbcf82ce8fff5650d80e1d
robisonwebdev <david@robisonweb.dev>
2022-03-09 10:31:46 -0700

    Imported Employment component. Added Employment component to form.'

--- src/components/Main/Form/Form.js
+++ src/components/Main/Form/Form.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Employment from './Employment';
 import PersonalInformation from './PersonalInformation';
 
 const Form = () => {
     return (
         <form>
             <PersonalInformation />
+            <Employment />
         </form>
     );
 };
