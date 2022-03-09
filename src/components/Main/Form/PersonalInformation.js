3ea64ab54bae22fc73d28b2924fa28212112af63
robisonwebdev <david@robisonweb.dev>
2022-03-09 10:19:38 -0700

    Imported useFormContext. Setup register. Gave section a className of .personal_information Inside section added strong tag and div tag. Added inner text to strong tag. Added multiple inputs to div tag.

--- src/components/Main/Form/PersonalInformation.js
+++ src/components/Main/Form/PersonalInformation.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useFormContext } from 'react-hook-form';
 
 const PersonalInformation = () => {
+    const { register } = useFormContext();
+
     return (
-        <section>PersonalInformation</section>
+        <section className='personal_information'>
+            <strong>Personal Information</strong>
+            <div>
+                <input {...register('firstName')} placeholder='First Name' />
+                <input {...register('lastName')} placeholder='Last Name' />
+                <input {...register('title')} placeholder='Title' />
+                <input {...register('city')} placeholder='City' />
+                <input {...register('state')} placeholder='State' />
+                <input {...register('zipCode')} placeholder='Zip Code' />
+                <input {...register('phoneNumber')} placeholder='Phone Number' />
+                <input {...register('email')} placeholder='Email' />
+                <input {...register('description')} placeholder='Description' />
+            </div>
+        </section>
     );
 };
 
