3c3b2c14ee3f8acb71fc9f719ea3bb2406636585
robisonwebdev <david@robisonweb.dev>
2022-03-09 11:22:38 -0700

    Destructured all watch items. Inside of .preview_header add strong tag and p tag. Set strong inner text to firstName lastName. Set p inner text to title.

--- src/components/Main/Preview/Preview.js
+++ src/components/Main/Preview/Preview.js
@@ -3,10 +3,34 @@ import { useFormContext } from 'react-hook-form';
 
 const Preview = () => {
     const { watch } = useFormContext();
+    const {
+        city,
+        company,
+        degree,
+        description,
+        educationCity,
+        educationFrom,
+        educationTo,
+        email,
+        employmentCity,
+        employmentFrom,
+        employmentTo,
+        firstName,
+        lastName,
+        phoneNumber,
+        position,
+        state,
+        title,
+        university,
+        zipCode
+    } = watch();
 
     return (
         <section className='preview'>
-            <section className='preview_header'></section>
+            <section className='preview_header'>
+                <strong>{`${firstName} ${lastName}`}</strong>    
+                <p>{title}</p>
+            </section>
             <section className='preview_main'>
                 <section className='preview_main_left'></section>
                 <section className='preview_main_right'></section>
