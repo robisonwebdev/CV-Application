e0be85e66249d06e35effe1ab305b77fd32663c4
robisonwebdev <david@robisonweb.dev>
2022-03-09 11:14:04 -0700

    Imported useFormContext Setup watch. Gave return section a className of .preview Inside .preview added two sections with classNames of .preview_header and .preview_main Inside .preview_main, added two sections with classNames of .preview_main_left and .preview_main_right

--- src/components/Main/Preview/Preview.js
+++ src/components/Main/Preview/Preview.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useFormContext } from 'react-hook-form';
 
 const Preview = () => {
+    const { watch } = useFormContext();
+
     return (
-        <section>Preview</section>
+        <section className='preview'>
+            <section className='preview_header'></section>
+            <section className='preview_main'>
+                <section className='preview_main_left'></section>
+                <section className='preview_main_right'></section>
+            </section>
+        </section>
     );
 };
 
