cb1cf5c21c027658b7042f980239d63e5b84c4fb
robisonwebdev <david@robisonweb.dev>
2022-03-10 10:56:27 -0700

    Imported PersonalInformation.css

--- src/components/Main/Form/PersonalInformation.js
+++ src/components/Main/Form/PersonalInformation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
+import '../../../styles/Main/Form/PersonalInformation.css';
 
 const PersonalInformation = () => {
     const { register } = useFormContext();
