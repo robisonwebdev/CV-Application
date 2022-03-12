c18393c93451745d9df8f93c1e81607eec85ac21
robisonwebdev <david@robisonweb.dev>
2022-03-12 13:56:19 -0700

    Imported useFormContext. Destructured watch from useFormContext. Destructured description from watch. Gave return section a className of .preview_description Inisde section added h3, hr, and p tag. Inside p tag, called description.

--- src/components/Main/Preview/PreviewDescription.js
+++ src/components/Main/Preview/PreviewDescription.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useFormContext } from 'react-hook-form';
 
 const PreviewDescription = () => {
+    const { watch } = useFormContext();
+    const { description } = watch();
+
     return (
-        <section>PreviewDescription</section>
+        <section className='preview_description'>
+            <h3>Description</h3>
+            <hr />
+            <p>{description}</p>
+        </section>
     );
 };
 
