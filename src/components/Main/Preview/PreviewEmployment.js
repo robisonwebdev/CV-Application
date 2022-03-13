d5ed7b4d25ba11f5e372e5bd4bafa3094a065783
robisonwebdev <david@robisonweb.dev>
2022-03-13 11:10:51 -0600

    Imported useFormContext. Destrucutred watch from useFormContext. Destructured company, employmentCity, employmentFrom, employmentTo, and position form watch. Gave return section a className of .preview_employment Inside return section added h3, hr, and div. Setup function displayInformation function that take a value and determines if value is an empty string. Returning true or false based on value.

--- src/components/Main/Preview/PreviewEmployment.js
+++ src/components/Main/Preview/PreviewEmployment.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { useFormContext } from 'react-hook-form';
 
 const PreviewEmployment = () => {
+    const { watch } = useFormContext();
+    const { company, employmentCity, employmentFrom, employmentTo, position } = watch();
+
+    const displayInformation = (value) => {
+        if (value === '') return false;
+
+        return true;
+    };
+
     return (
-        <section>PreviewEmployment</section>
+        <section className='preview_employment'>
+            <h3>Employment</h3>
+            <hr />
+            <div>
+                <div>
+                    <p>{`${displayInformation(employmentFrom) ? employmentFrom : 'Start Date'} - ${displayInformation(employmentTo) ? employmentTo : 'End Date'}`}</p>
+                </div>
+                <div>
+                    <p>{displayInformation(position) ? position : 'Position'}</p>
+                    <p>{`${displayInformation(company) ? company : 'Company'}, ${displayInformation(employmentCity) ? employmentCity : 'City'}`}</p>
+                </div>
+            </div>
+        </section>
     );
 };
 
