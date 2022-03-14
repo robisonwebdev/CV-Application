2a9deccbb0bc21460c7b30b3b97a2ada69987e91
robisonwebdev <david@robisonweb.dev>
2022-03-14 15:10:07 -0600

    Imported useFormContext. Destructured watch from useFormContext. Destructured degree, educationCity, educationFrom, educationTo, and university from watch. Gave return section a className of .preview_education Added h3, hr, and div to return section. Setup function called displayInformation to determine if information should be displayed.

--- src/components/Main/Preview/PreviewEducation.js
+++ src/components/Main/Preview/PreviewEducation.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { useFormContext } from 'react-hook-form';
 
 const PreviewEducation = () => {
+    const { watch } = useFormContext();
+    const { degree, educationCity, educationFrom, educationTo, university } = watch();
+
+    const displayInformation = (value) => {
+        if (value === '') return false;
+
+        return true;
+    };
+
     return (
-        <section>PreviewEducation</section>
+        <section className='preview_education'>
+            <h3>Education</h3>
+            <hr />
+            <div>
+                <p>{`${displayInformation(educationFrom) ? educationFrom : 'Start Date'} - ${displayInformation(educationTo) ? educationTo : 'End Date'}`}</p>
+                <div>
+                    <p>{`${displayInformation(university) ? university : 'University'}, ${displayInformation(educationCity) ? educationCity : 'City'}`}</p>
+                    <p>{`Degree: ${displayInformation(degree) ? degree : 'Degree'}`}</p>
+                </div>
+            </div>
+        </section>
     );
 };
 
