f775eb7ef4df70002e51faac99305aa63dc60c2e
robisonwebdev <david@robisonweb.dev>
2022-03-14 15:07:47 -0600

    Imported PreviewEducation component. Called PreviewEducation in .preview_main_left

--- src/components/Main/Preview/Preview.js
+++ src/components/Main/Preview/Preview.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PreviewDescription from './PreviewDescription';
+import PreviewEducation from './PreviewEducation';
 import PreviewEmployment from './PreviewEmployment';
 import PreviewHeader from './PreviewHeader';
 import { useFormContext } from 'react-hook-form';
@@ -8,14 +9,9 @@ const Preview = () => {
     const { watch } = useFormContext();
     const {
         city,
-        degree,
-        educationCity,
-        educationFrom,
-        educationTo,
         email,
         phoneNumber,
         state,
-        university,
         zipCode
     } = watch();
 
@@ -25,7 +21,8 @@ const Preview = () => {
             <section className='preview_main'>
                 <section className='preview_main_left'>
                     <PreviewDescription />
-                    <PreviewEmployment />  
+                    <PreviewEmployment />
+                    <PreviewEducation /> 
                 </section>
                 <section className='preview_main_right'></section>
             </section>
