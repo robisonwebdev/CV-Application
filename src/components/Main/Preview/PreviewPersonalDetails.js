b5b1e52b699b10c476555822c9a1ce2d1a2e845b
robisonwebdev <david@robisonweb.dev>
2022-03-15 13:25:57 -0600

    Imported useFormContext. Destructured watch from useFormContext. Destructured city, email, phoneNumber, state, and zipCode from watch. Setup displayInformation function to determine if value passed should be displayed in return. Gave return section a className of .preview_personal_details. Setup structure of component inside return section.

--- src/components/Main/Preview/PreviewPersonalDetails.js
+++ src/components/Main/Preview/PreviewPersonalDetails.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { useFormContext } from 'react-hook-form';
 
 const PreviewPersonalDetails = () => {
+    const { watch } = useFormContext();
+    const { city, email, phoneNumber, state, zipCode } = watch();
+
+    const displayInformation = (value) => {
+        if (value === '') return false;
+
+        return true;
+    };
+
     return (
-        <section>PreviewPersonalDetails</section>
+        <section className='preview_personal_details'>
+            <h3>Personal details</h3>
+            <hr />
+            <div>
+                <h4>Address</h4>
+                <p>{`${displayInformation(city) ? city : 'City'}, ${displayInformation(state) ? state : 'State'} ${displayInformation(zipCode) ? zipCode : 'Zip Code'}`}</p>
+            </div>
+            <div>
+                <h4>Phone Number</h4>
+                <p>{`${displayInformation(phoneNumber) ? phoneNumber : 'xxx-xxx-xxxx'}`}</p>
+            </div>
+            <div>
+                <h4>Email</h4>
+                <p>{`${displayInformation(email) ? email : 'yourEmail@email.com'}`}</p>
+            </div>
+        </section>
     );
 };
 
