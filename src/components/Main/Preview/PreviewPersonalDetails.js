1f31d969eeb6ed3783ddecdbd8f6588b36b4bdad
robisonwebdev <david@robisonweb.dev>
2022-03-15 13:14:08 -0600

    Setup initial functional PreviewPersonalDetails component.

---
+++ src/components/Main/Preview/PreviewPersonalDetails.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PreviewPersonalDetails = () => {
+    return (
+        <section>PreviewPersonalDetails</section>
+    );
+};
+
+export default PreviewPersonalDetails;
