0d2c871a995804b4797c84658bd3c5694c9ae09b
robisonwebdev <david@robisonweb.dev>
2022-03-15 13:27:58 -0600

    Imported PreviewPersonalDetails.css

--- src/components/Main/Preview/PreviewPersonalDetails.js
+++ src/components/Main/Preview/PreviewPersonalDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
+import '../../../styles/Main/Preview/PreviewPersonalDetails.css';
 
 const PreviewPersonalDetails = () => {
     const { watch } = useFormContext();
