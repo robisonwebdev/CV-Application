c8f95d20c575b9b99ae908e4d5aa2e1be6a03d78
robisonwebdev <david@robisonweb.dev>
2022-03-16 14:44:15 -0600

    Setup displayInformation function to determine if value pass into should display it information. Setup ternarys for h1 and p tag.

--- src/components/Main/Preview/PreviewHeader.js
+++ src/components/Main/Preview/PreviewHeader.js
@@ -4,12 +4,18 @@ import '../../../styles/Main/Preview/PreviewHeader.css'
 
 const PreviewHeader = () => {
     const { watch } = useFormContext();
-    const { firstName, lastName, title} = watch()
+    const { firstName, lastName, title} = watch();
+
+    const displayInformation = (value) => {
+        if (value === '') return false;
+
+        return true;
+    };
 
     return (
         <section className='preview_header'>
-            <h1>{`${firstName} ${lastName}`}</h1>
-            <p>{title}</p>
+            <h1>{`${displayInformation(firstName) ? firstName : 'First Name'} ${displayInformation(lastName) ? lastName : 'Last Name'}`}</h1>
+            <p>{displayInformation(title) ? title : 'Title'}</p>
         </section>
     );
 };
